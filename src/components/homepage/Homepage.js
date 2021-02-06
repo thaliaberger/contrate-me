f67cc9ccc5b370a4d439720bdaee468acc02d5b6
Lorena <lorena@proesc.com>
2021-02-06 00:44:21 -0300

    Criado html da seção de ação

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -17,17 +17,24 @@ function Homepage() {
    
       </header>
 
+      <section className="call-to-action">
+        <div className="action-content">
+          <h1>Pronto para se destacar no recrutamento e entrar no mercado?</h1>
+          <a href="https://contrate-me.vercel.app/" alt="Testar" className="home-btn">Provar meu conhecimento</a>
+        </div>
+        <img src={headerImage} />
+      </section>
       <footer>
         <img src={headerImage} />
         <div className="Menu">
-          <h1>Menu</h1>
+          <h3>Menu</h3>
           <a href="https://contrate-me.vercel.app/" alt="Como funciona">Como funciona</a>
           <a href="https://contrate-me.vercel.app/" alt="Dúvidas">Dúvidas</a>
           <a href="https://contrate-me.vercel.app/" alt="Empresas">Empresas</a>
           <a href="https://contrate-me.vercel.app/" alt="Entrar">Entrar</a>
         </div>
         <div className="Suporte">
-          <h1>Suporte</h1>
+          <h3>Suporte</h3>
           <a href="https://contrate-me.vercel.app/" alt="Email">contato@mecontrate.com</a>
           <a href="https://contrate-me.vercel.app/" alt="Ajuda & Suporte">Ajuda & Suporte</a>
           <a href="https://contrate-me.vercel.app/" alt="Politica de privacidade">Politica de privacidade</a>
