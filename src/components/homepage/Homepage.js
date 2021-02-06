44d6c37c05253aca107956b92e231d3b9bef1885
Lorena <lorena@proesc.com>
2021-02-06 00:33:57 -0300

    Criado html do footer

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -16,6 +16,24 @@ function Homepage() {
   <h1></h1></div>
    
       </header>
+
+      <footer>
+        <img src={headerImage} />
+        <div className="Menu">
+          <h1>Menu</h1>
+          <a href="https://contrate-me.vercel.app/" alt="Como funciona">Como funciona</a>
+          <a href="https://contrate-me.vercel.app/" alt="Dúvidas">Dúvidas</a>
+          <a href="https://contrate-me.vercel.app/" alt="Empresas">Empresas</a>
+          <a href="https://contrate-me.vercel.app/" alt="Entrar">Entrar</a>
+        </div>
+        <div className="Suporte">
+          <h1>Suporte</h1>
+          <a href="https://contrate-me.vercel.app/" alt="Email">contato@mecontrate.com</a>
+          <a href="https://contrate-me.vercel.app/" alt="Ajuda & Suporte">Ajuda & Suporte</a>
+          <a href="https://contrate-me.vercel.app/" alt="Politica de privacidade">Politica de privacidade</a>
+          <a href="https://contrate-me.vercel.app/" alt="Termos & Condições">Termos & Condições</a>
+        </div>
+      </footer>
     </div>
   );
 }
