bdc0361c61abfa24d3965b1d39bc5461376697fe
Lorena <lorena@proesc.com>
2021-02-06 19:49:11 -0300

    Criado formulário para cadastro de novo desafio

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import "./App.css";
 import Homepage from "../src/components/homepage/Homepage";
 import BusinessHome from "../src/components/business/BusinessHome";
 import Candidate from "../src/components/candidate/Candidate";
+import NewChallenge from "../src/components/new_challenge/NewChallenge";
 
 function App() {
   return (
     <div className="container">
       <BrowserRouter>
         <Switch>
+          {/* Candidato */}
           <Route exact path="/" component={Homepage} />
-          <Route exact path="/empresa" component={BusinessHome} />
           <Route exact path="/candidato" component={Candidate} />
+          {/* <Route path="/desafios" component={Desafios} /> */}
+
+          {/* Empresa */}
+          <Route exact path="/empresa" component={BusinessHome} />
+          <Route path="/novo_desafio" component={NewChallenge} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/components/new_challenge/NewChallenge.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+
+function NewChallenge() {
+  const [state, setState] = useState({
+    nome: "",
+    descricao: "",
+    linkExterno: "",
+    empresaId: "",
+  });
+
+  const history = useHistory();
+
+  function handleChange(event) {
+    setState({
+      ...state,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await axios.post(
+      "https://contrate-me-api.herokuapp.com/challenges",
+      state
+    );
+
+    console.log(response);
+    history.push("/");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Teste</h1>
+      {/* Nome */}
+      <div className="">
+        <label htmlFor="novoNome">Nome</label>
+        <input
+          type="text"
+          className=""
+          id="novoNome"
+          name="nome"
+          onChange={handleChange}
+          value={state.nome}
+        />
+      </div>
+
+      {/* Data de Nascimento */}
+      <div className="">
+        <label htmlFor="novaDescricao">Descrição</label>
+        <input
+          type="text"
+          className=""
+          id="novaDescricao"
+          name="descricao"
+          onChange={handleChange}
+          value={state.descricao}
+        />
+      </div>
+
+      {/* Email */}
+      <div className="">
+        <label htmlFor="novoLinkExterno">Link Externo</label>
+        <input
+          type="text"
+          className=""
+          id="novoLinkExterno"
+          name="linkExterno"
+          onChange={handleChange}
+          value={state.linkExterno}
+        />
+      </div>
+
+      <button type="submit" className="">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default NewChallenge;
