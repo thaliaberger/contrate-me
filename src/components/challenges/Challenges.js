760dee5d505bf053e56c6034a646a08af597ae7a
Lorena <lorena@proesc.com>
2021-02-07 11:01:25 -0300

    Criado menu lateral para candidatos

--- src/components/challenges/Challenges.js
+++ src/components/challenges/Challenges.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import api from '../../services/api';
-import Sidebar from "../sidebar/Sidebar";
+import Sidebar from "../sidebar_candidate/SidebarCandidate";
 import Header from "../header/Header";
 import './Challenges.css';
 
@@ -28,9 +28,9 @@ function Challenges() {
                 {
                     challenges.map(challenge =>(
                         <div className="challenge">
-                            <h1>{challenge.nome}</h1>
-                            <h1>{challenge.descricao}</h1>
-                            <h1>{challenge.linkExterno}</h1>
+                            <h3>{challenge.nome}</h3>
+                            <p>{challenge.descricao}</p>
+                            <a href={challenge.linkExterno} alt="Link externo">Link externo</a>
                         </div>
                     ))
                 }
